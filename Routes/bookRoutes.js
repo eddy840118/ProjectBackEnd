ef98f35515cdc05ff4ef507269ab548b7fbaf419
ArsMkoi <ryzonix@mail.fresnostate.edu>
2018-05-06 11:14:21 -0700

    userRoutes added, login authentication partially implemented

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -1,11 +1,11 @@
 var express = require('express');
 
 
-var routes = function(Book){
+var routes = function (Book) {
     var bookRouter = express.Router();
 
     bookRouter.route('/')
-        .post(function(req, res){
+        .post(function (req, res) {
             var book = new Book(req.body);
 
 
@@ -13,82 +13,93 @@ var routes = function(Book){
             res.status(201).send(book);
 
         })
-        .get(function(req,res){
+        .get(function (req, res) {
 
             var query = {};
 
-            if(req.query.genre)
-            {
+            if (req.query.genre) {
                 query.genre = req.query.genre;
             }
-            Book.find(query, function(err,books){
-                if(err)
+            Book.find(query, function (err, books) {
+                if (err)
                     res.status(500).send(err);
                 else
                     res.json(books);
             });
         });
 
-    bookRouter.use('/:bookId', function(req,res,next){
-        Book.findById(req.params.bookId, function(err,book){
-            if(err)
+    bookRouter.use('/id/:bookId', function (req, res, next) {
+        Book.findById(req.params.bookId, function (err, book) {
+            if (err)
                 res.status(500).send(err);
-            else if(book)
-            {
+            else if (book) {
                 req.book = book;
                 next();
             }
-            else
-            {
+            else {
                 res.status(404).send('no book found');
             }
         });
     });
-    bookRouter.route('/:bookId')
-        .get(function(req,res){
+    bookRouter.route('/id/:bookId')
+        .get(function (req, res) {
 
             res.json(req.book);
 
         })
-        .put(function(req,res){
+        .put(function (req, res) {
             req.book.title = req.body.title;
             req.book.author = req.body.author;
             req.book.genre = req.body.genre;
             req.book.read = req.body.read;
-            req.book.save(function(err){
-                if(err)
+            req.book.save(function (err) {
+                if (err)
                     res.status(500).send(err);
-                else{
+                else {
                     res.json(req.book);
                 }
             });
         })
-        .patch(function(req,res){
-            if(req.body._id)
+        .patch(function (req, res) {
+            if (req.body._id)
                 delete req.body._id;
 
-            for(var p in req.body)
-            {
+            for (var p in req.body) {
                 req.book[p] = req.body[p];
             }
 
-            req.book.save(function(err){
-                if(err)
+            req.book.save(function (err) {
+                if (err)
                     res.status(500).send(err);
-                else{
+                else {
                     res.json(req.book);
                 }
             });
         })
-        .delete(function(req,res){
-            req.book.remove(function(err){
-                if(err)
+        .delete(function (req, res) {
+            req.book.remove(function (err) {
+                if (err)
                     res.status(500).send(err);
-                else{
+                else {
                     res.status(204).send('Removed');
                 }
             });
         });
+
+    bookRouter.route('/get-all-books-for-user')
+        .post(function (req, res) {
+
+            Book.find({ user_id: req.body.userID }, function (err, books) {
+                console.log('from get all books for user err: ', err, 'books: ', books);
+                if (err) {
+                    res.status(500).send(err);
+                } else if (books.length == 0) {
+                    res.status(201).send({ errorMessage: "Book not found or no books in the database yet!" });
+                } else {
+                    res.status(201).send(books);
+                }
+            });
+        })
     return bookRouter;
 };
 
